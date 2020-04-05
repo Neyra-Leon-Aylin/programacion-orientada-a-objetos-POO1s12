e009704ee884893c1acb2f50d38eb541c9b0f28b
Neyra-Leon-Aylin <63180520+Neyra-Leon-Aylin@users.noreply.github.com>
2020-04-04 23:40:07 -0600

    programacion orientada a objetos
    practicas de la materia programacion orientada a objetos

---
+++ ProgALN002.java
@@ -0,0 +1,96 @@
+
+package progaln002;
+
+import java.util.*;
+
+public class ProgALN002 {
+
+    public static void main(String[] args) {
+     
+        Scanner leer=new Scanner (System.in);
+ 
+ System.out.print("v.conumnas: ");
+ int n = leer.nextInt();
+  System.out.print("v.filas: ");
+ int m = leer.nextInt();
+ 
+ System.out.println("matriz 1");
+ 
+ String[][]m1=new String [n][m];
+ String[][]m2=new String [n][m];
+ String[][]m3=new String [n][m];
+ 
+ for (int i=0;i<n;i++){
+  for (int j=0;j<m;j++){
+      if (i==0||j==0||i==n-1||j==m-1 ){
+          
+      m1[i][j]="1";
+  }
+  else{
+          m1[i][j]="0";
+          }          
+ }
+
+}
+    for (int i = 0; i< n;i++) {
+        for (int j = 0; j< m;j++){
+            System.out.print(m1[i][j]+" ");
+        }
+        System.out.println(" ");
+    }
+    System.out.println("\n");
+    
+ 
+  System.out.println("matriz 2");  
+  System.out.println("\n");
+  
+  for (int i=0;i<n;i++){
+  for (int j=0;j<m;j++){
+      if (i==j||j==(m-(i+1)) ){
+          
+      m2[i][j]="1";
+  }
+  else{
+          m2[i][j]="0";
+          }          
+ }
+    
+}
+    for (int i = 0; i< n;i++) {
+        for (int j = 0; j< m;j++){
+            System.out.print(m2[i][j]+" ");
+        }
+        System.out.println(" ");
+    }
+    System.out.println("\n");
+   
+  System.out.println("matriz 3");  
+  System.out.println("\n");
+  
+  for (int i=0;i<n;i++){
+  for (int j=0;j<m;j++){
+      if (i+j<n ){
+           m3[i][j]="0";
+  }
+    if (i+j==(m-1))
+           m3[i][j]=" ";
+  
+    if (i+j>=n){
+           m3[i][j]="2";
+  }
+         
+ }
+    
+}
+    for (int i = 0; i< n;i++) {
+        for (int j = 0; j< m;j++){
+            System.out.print(m3[i][j]+" ");
+        }
+        System.out.println(" ");
+    }
+    System.out.println("\n");
+    
+    
+    }
+    
+}
---
+++ ProgANL003.java
@@ -0,0 +1,44 @@
+
+package proganl003;
+
+import java.util.Scanner;
+
+public class ProgANL003 {
+
+  
+    public static void main(String[] args) {
+          Scanner leer = new Scanner(System.in);
+
+        System.out.print("v.conumnas: ");
+        int n = leer.nextInt();
+        System.out.print("v.filas: ");
+        int m = leer.nextInt(); 
+
+        String[][] m1 = new String[n][m];
+
+         System.out.println("\n");
+  
+  for (int i=0;i<n;i++){
+  for (int j=0;j<m;j++){
+      if (i==j||j==(m-(i+1)) ){
+          
+      m1[i][j]=" ";
+      
+  }
+  else{
+          m1[i][j]="0";
+          }          
+ }
+    
+}
+    for (int i = 0; i< n;i++) {
+        for (int j = 0; j< m;j++){
+            System.out.print(m1[i][j]+" ");
+        }
+        System.out.println(" ");
+    }
+    System.out.println("\n");
+    
+    }
+}
+    
